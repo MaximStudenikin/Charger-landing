8de452e746746f2caebdc4e4c0c4f3f93de06643
Максим Cтуденикин <operatoribm@icloud.com>
2018-04-14 17:52:02 +0300

    работа над ошибками

--- dev/js/common.js
+++ dev/js/common.js
@@ -36,61 +36,61 @@ $(document).ready(function () {
 
     //click ruls btn welcome slider
 
-    $('.controls').on('click', function (event) {
-        event.preventDefault();
+    // $('.controls').on('click', function (event) {
+    //     event.preventDefault();
 
-        var $this = $(event.target),
-            cont = $this.parents().find('.slider'),
-            browsing = $this.closest('.welcome--slider__bg', cont),
-            items = $('[data-slider_item]', cont),
-            activeItem = items.filter('.active__slide');
+    //     var $this = $(event.target),
+    //         cont = $this.parents().find('.slider'),
+    //         browsing = $this.closest('.welcome--slider__bg', cont),
+    //         items = $('[data-slider_item]', cont),
+    //         activeItem = items.filter('.active__slide');
 
-        var existedItem,
-            edgeItem,
-            reqItem;
+    //     var existedItem,
+    //         edgeItem,
+    //         reqItem;
 
-        if ($this.hasClass('btn__next')) {
-            existedItem = activeItem.next();
-            var backImg = activeItem.find('.slider__img').attr('src');
-            browsing.css('background', 'url(' + backImg + ') no-repeat');
-            browsing.css('backgroundSize', 'cover');
-            edgeItem = items.first();
-        }
+    //     if ($this.hasClass('btn__next')) {
+    //         existedItem = activeItem.next();
+    //         var backImg = activeItem.find('.slider__img').attr('src');
+    //         browsing.css('background', 'url(' + backImg + ') no-repeat');
+    //         browsing.css('backgroundSize', 'cover');
+    //         edgeItem = items.first();
+    //     }
 
-        if ($this.hasClass('btn__prev')) {
-            cont = $this.parentsUntil().find('.slider');
-            existedItem = activeItem.prev();
-            var backImg = activeItem.find('.slider__img').attr('src');
-            browsing.css('background', 'url(' + backImg + ') no-repeat');
-            browsing.css('backgroundSize', 'cover');
-            edgeItem = items.last();
-        };
+    //     if ($this.hasClass('btn__prev')) {
+    //         cont = $this.parentsUntil().find('.slider');
+    //         existedItem = activeItem.prev();
+    //         var backImg = activeItem.find('.slider__img').attr('src');
+    //         browsing.css('background', 'url(' + backImg + ') no-repeat');
+    //         browsing.css('backgroundSize', 'cover');
+    //         edgeItem = items.last();
+    //     };
 
-        reqItem = existedItem.length ? existedItem.index() : edgeItem.index();
+    //     reqItem = existedItem.length ? existedItem.index() : edgeItem.index();
 
-        moveSlide(cont, reqItem);
+    //     moveSlide(cont, reqItem);
 
-    });
+    // });
 
     //click ruls btn reviews slider
 
-    $('.reviews__controls').on('click', function (event) {
+    $('.slider__controls').on('click', function (event) {
         event.preventDefault();
 
         var $this = $(event.target),
-            cont = $this.closest('.reviews__slider'),
+            cont = $this.closest('[data-slider]'),
             items = $('[data-slider_item]', cont),
             activeItem = items.filter('.active__slide');
         var existedItem,
             edgeItem,
             reqItem;
 
-        if ($this.hasClass('btn__next')) {
+        if ($this.hasClass('slider__btn_next')) {
             existedItem = activeItem.next();
             edgeItem = items.first();
         }
 
-        if ($this.hasClass('btn__prev')) {
+        if ($this.hasClass('slider__btn_prev')) {
             existedItem = activeItem.prev();
             edgeItem = items.last();
         }
