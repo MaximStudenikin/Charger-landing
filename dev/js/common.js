f7cec8b7f5efdff0584515f624e773526eaa14b6
Максим Cтуденикин <operatoribm@icloud.com>
2018-04-15 13:35:52 +0300

    работа на адаптивом первого экрана

--- dev/js/common.js
+++ dev/js/common.js
@@ -12,75 +12,26 @@ $(document).ready(function () {
 
     //slider
 
-    //searh number slid and activ slide
+    // slider back images 
 
-    var moveSlide = function (cont, slideNum) {
+    var browsing = function (container, activeSlide) {
+        var showWindow = container.parents().find('.slider-bg'),
+            backImg = activeSlide.find('.slider__img').attr('src');
 
-        var items = cont.find('[data-slider_item]'),
-            activeSlide = items.filter('.active__slide'),
-            reqItem = items.eq(slideNum),
-            reqIndex = reqItem.index(),
-            list = cont.find('[data-slider_list]'),
-            dur = 500;
-
-        if (reqItem.length) {
-            list.animate({
-                'left': -reqIndex * 100 + '%'
-            }, dur, function () {
-                activeSlide.removeClass('active__slide');
-                reqItem.addClass('active__slide');
-            });
+        showWindow.css('background', 'url(' + backImg + ') no-repeat');
+        showWindow.css('backgroundSize', 'cover');
+            
         }
-
-    };
-
-    //click ruls btn welcome slider
-
-    // $('.controls').on('click', function (event) {
-    //     event.preventDefault();
-
-    //     var $this = $(event.target),
-    //         cont = $this.parents().find('.slider'),
-    //         browsing = $this.closest('.welcome--slider__bg', cont),
-    //         items = $('[data-slider_item]', cont),
-    //         activeItem = items.filter('.active__slide');
-
-    //     var existedItem,
-    //         edgeItem,
-    //         reqItem;
-
-    //     if ($this.hasClass('btn__next')) {
-    //         existedItem = activeItem.next();
-    //         var backImg = activeItem.find('.slider__img').attr('src');
-    //         browsing.css('background', 'url(' + backImg + ') no-repeat');
-    //         browsing.css('backgroundSize', 'cover');
-    //         edgeItem = items.first();
-    //     }
-
-    //     if ($this.hasClass('btn__prev')) {
-    //         cont = $this.parentsUntil().find('.slider');
-    //         existedItem = activeItem.prev();
-    //         var backImg = activeItem.find('.slider__img').attr('src');
-    //         browsing.css('background', 'url(' + backImg + ') no-repeat');
-    //         browsing.css('backgroundSize', 'cover');
-    //         edgeItem = items.last();
-    //     };
-
-    //     reqItem = existedItem.length ? existedItem.index() : edgeItem.index();
-
-    //     moveSlide(cont, reqItem);
-
-    // });
-
     //click ruls btn reviews slider
 
     $('.slider__controls').on('click', function (event) {
         event.preventDefault();
 
         var $this = $(event.target),
-            cont = $this.closest('[data-slider]'),
+            cont = $this.parents().find('[data-slider]'),
             items = $('[data-slider_item]', cont),
             activeItem = items.filter('.active__slide');
+            
         var existedItem,
             edgeItem,
             reqItem;
@@ -98,7 +49,39 @@ $(document).ready(function () {
         reqItem = existedItem.length ? existedItem.index() : edgeItem.index();
 
         moveSlide(cont, reqItem);
+        browsing(cont, activeItem);
 
     });
+
+    //searh number slid and activ slide
+
+    var moveSlide = function (cont, slideNum) {
+
+        var items = cont.find('[data-slider_item]'),
+            activeSlide = items.filter('.active__slide'),
+            reqItem = items.eq(slideNum),
+            reqIndex = reqItem.index(),
+            list = cont.find('[data-slider_list]'),
+            dur = 500;
+
+        if (reqItem.length) {
+            list.animate({
+                'left': -reqIndex * 100 + '%',
+            }, dur, function () {
+                activeSlide.removeClass('active__slide');
+                reqItem.addClass('active__slide');
+            });
+        }
+    };
+
+
+
+
+    var container = $('[data-slider]'),
+        activeItem = container.find('.active__slide');
+
+    browsing(container, activeItem);
+
+
 
 });
